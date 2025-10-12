a84158007249f8a73cac7cca5c1a46e0ca7cd8b3
AZaki02 <73823698+AZaki02@users.noreply.github.com>
2025-10-12 21:50:56 +1100

    Add files via upload

---
+++ choropleth_loader 2.js
@@ -0,0 +1,4 @@
+var vg_1 = "choropleth-modal-venues.json";
+vegaEmbed("#choropleth2", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
