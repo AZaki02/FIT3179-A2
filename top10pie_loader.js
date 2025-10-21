107df600185fdf36a54416036399fac3f41db59d
AZaki02 <73823698+AZaki02@users.noreply.github.com>
2025-10-21 11:59:32 +1100

    Add files via upload

---
+++ top10pie_loader.js
@@ -0,0 +1,4 @@
+var vg_1 = "top-10-genres-pie.json";
+vegaEmbed("#piechart", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
